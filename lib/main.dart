9afcb3476a98380f14f5a80d00db55c3ec2d44c1
Heumou-Fraidy <heudan@gmail.com>
2025-04-15 14:47:03 +0100

    Updating such that it uppdates medical news

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:news_app_using_riverpod/pages/home_page.dart';
-import 'package:flutter/services.dart';
 
 void main() {
   runApp(
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:news_app_using_riverpod/constants/constants.dart';
 
 class NewsService {
-
-   final Dio _dio = Dio(
+  final Dio _dio = Dio(
     BaseOptions(
       baseUrl: ApiUrls.baseUrl,
       responseType: ResponseType.json
@@ -11,18 +10,14 @@ class NewsService {
   );
 
   fetchNews() async {
-
-    var response = await _dio.get('v2/top-headlines?country=us&apiKey=094abddfda1944fb87d868677f3005ee');
-
+    // Modified to only fetch health news
+    var response = await _dio.get('v2/top-headlines?category=health&country=us&apiKey=094abddfda1944fb87d868677f3005ee');
     return response.data;
-
   }
   
   fetchNewsBySearching(String title) async {
-
-    var response = await _dio.get('v2/everything?q='+title+'&apiKey=094abddfda1944fb87d868677f3005ee');
-
+    // Modified to search within health topics
+    var response = await _dio.get('v2/everything?q=${title}+tips&apiKey=094abddfda1944fb87d868677f3005ee');
     return response.data;
   }
-
 }
